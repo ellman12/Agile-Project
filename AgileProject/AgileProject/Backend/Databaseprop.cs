576e67c3f0577175ef87dcd7f1b6bbfcd1580407
Radicalbot1 <Radicalbot@icloud.com>
2023-03-25 16:06:45 -0500

    Made initial record objects
    Created the start to the Card, Set, and Folder records. These are currently found in the BackendTest folder so I can test them. I added four tests to check their functionality.

---
+++ AgileProject/AgileProject/Backend/Databaseprop.cs
@@ -0,0 +1,27 @@
+﻿namespace AgileProject.Backend
+{
+    public record Card
+    {
+        public string Front { get; set; }
+        public string Back { get; set; }
+
+        /*
+        //ID if needed
+        public string ID { get; set; }
+        //Name if needed
+        public string Name { get; set; }
+        */
+    }
+
+    public record Set
+    {
+        public string SetName { get; set; }
+        public List<Card> Cards { get; set;}
+    }
+
+    public record Folder
+    {
+        public string FolderName { get; set; }
+        public List<Set> Sets { get; set;}
+    }
+}
---
+++ AgileProject/AgileProjectTest/BackendTest/DatabasepropTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProjectTest.BackendTest
+{
+    public record Card
+    {
+        public Card(string front, string back)
+        {
+            Front = front;
+            Back = back;
+        }
+
+        public string Front { get; set; }
+        public string Back { get; set; }
+
+        private bool Correct = false;
+
+        /*
+        //ID if needed
+        public string ID { get; set; }
+        //Name if needed
+        public string Name { get; set; }
+        */
+    }
+
+    public record Set
+    {
+        public string SetName { get; set; }
+        public List<Card> Cards { get; set; }
+
+        public Set()
+        {
+            Cards = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            try
+            {
+                Cards.Add(card);
+            }
+            catch 
+            {
+                throw new Exception("Failed to add card");
+            }
+        }
+
+        public void RemoveCard(Card card) 
+        {
+            try
+            {
+                Cards.Remove(card);
+            }
+            catch
+            {
+                throw new Exception("Failed to remove card");
+            }
+        }
+
+        public Card GetCard(int x)
+        {
+            return Cards[x];
+        }
+
+        public int Size() 
+        {
+            return Cards.Count;
+        }
+    }
+
+    public record Folder
+    {
+        public string FolderName { get; set; }
+        public List<Set> Sets { get; set; }
+
+        public Folder(string name)
+        {
+            FolderName = name;
+            Sets = new List<Set>();
+        }
+
+        public void AddSet(Set s)
+        {
+            try
+            {
+                Sets.Add(s);
+            }
+            catch
+            {
+                throw new Exception("Failed to add Set");
+            }
+        }
+
+        public void RemoveSetByPlace(int x)
+        {
+            Sets.RemoveAt(x);
+        }
+
+        public void RemoveSetByName(string name)
+        {
+            for(int i = 0; i < Sets.Count; i++)
+            {
+                if(Sets[i].SetName == name)
+                {
+                    Sets.RemoveAt(i);
+                }
+            }
+        }
+
+        public int Size()
+        {
+            return Sets.Count;
+        }
+    }
+}
